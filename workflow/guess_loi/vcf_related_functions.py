4f429c5a2f46bf9dfaa990d11eec450f96629afa
Paolo Martini <paolo.cave@gmail.com>
2019-02-20 12:02:57 +0100

    Additional option to set up genotipe sample name

--- workflow/guess_loi/vcf_related_functions.py
+++ workflow/guess_loi/vcf_related_functions.py
@@ -8,13 +8,14 @@ def reformat_vcf_with_het_genotype():
                 """)
     parser.add_argument('output', help="output file name")
     parser.add_argument('vcf', help="vcfs of the SNPs")
+    parser.add_argument('--sampleName', help="the header name for GT column", default="gentype")
 
     args = parser.parse_args()
 
-    annotate_vcf_with_heterozygous_genotype(args.vcf, args.output)
+    annotate_vcf_with_heterozygous_genotype(args.vcf, args.output, args.sampleName)
 
 
-def annotate_vcf_with_heterozygous_genotype(vcf, vcf_out):
+def annotate_vcf_with_heterozygous_genotype(vcf, vcf_out, sampleName):
     with open(vcf, "rt") as vf, open(vcf_out, "wt") as vo:
         for line_idx, line in enumerate(vf):
             if line[:2] == "##":
@@ -22,7 +23,7 @@ def annotate_vcf_with_heterozygous_genotype(vcf, vcf_out):
             else:
                 tokens = line.rstrip().split('\t')
                 if line[:1] == "#":
-                    new_line = tokens[:9] + ["meta_sample\n"]  # type: List
+                    new_line = tokens[:9] + [sampleName + "\n"]  # type: List
                 else:
                     new_line = tokens[:8] + ["GT", "0/1\n"]  # type: List
 
